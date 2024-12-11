f9dfba1d9161b128441bed63ec6b48687a5538b3
Victor <vmmrcas98@gmail.com>
2024-12-11 09:14:43 +0100

    hola

--- reader.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';//Require package.json
-
-try {
-    const data = fs.readFileSync('./quijote.txt', 'utf8');
-    console.log(data);
-} catch (err) {
-    console.error(err);
-}
